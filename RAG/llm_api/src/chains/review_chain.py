8a47f78d885f33337941a520fefa9fb1787eca59
fahmiaziz98 <fahmiazizfadhil09@gmail.com>
2024-05-28 17:25:55 +0700

    init

--- RAG/llm_api/src/chains/review_chain.py
+++ RAG/llm_api/src/chains/review_chain.py
@@ -1,7 +1,7 @@
 import os
 import logging
 import time
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 from langchain_community.vectorstores.neo4j_vector import Neo4jVector
 from langchain_cohere import CohereEmbeddings, ChatCohere
 from langchain.chains.retrieval_qa.base import RetrievalQA
@@ -12,7 +12,7 @@ from langchain.prompts import (
     HumanMessagePromptTemplate,
 )
 
-load_dotenv()
+load_dotenv(find_dotenv())
 HOSPITAL_QA_MODEL = os.getenv("HOSPITAL_QA_MODEL")
 COHERE_API_KEY = os.getenv("COHERE_API_KEY")
 NEO4J_URI = os.getenv("NEO4J_URI")
@@ -24,21 +24,27 @@ logger = logging.getLogger(__name__)
 
 def setup_vector_chain():
     logger.info("Setting up vector chain...")
-    neo4j_vector_index = Neo4jVector.from_existing_graph(
-        embedding=CohereEmbeddings(cohere_api_key=COHERE_API_KEY),
-        url=NEO4J_URI,
-        username=NEO4J_USERNAME,
-        password=NEO4J_PASSWORD,
-        index_name="reviews",
-        node_label="Review",
-        text_node_properties=[
-            "physician_name",
-            "patient_name",
-            "text",
-            "hospital_name",
-        ],
-        embedding_node_property="embedding",
-    )
+
+    try:
+        neo4j_vector_index = Neo4jVector.from_existing_graph(
+            embedding=CohereEmbeddings(cohere_api_key=COHERE_API_KEY),
+            url=NEO4J_URI,
+            username=NEO4J_USERNAME,
+            password=NEO4J_PASSWORD,
+            search_type="hybrid",
+            index_name="reviews",
+            node_label="Review",
+            text_node_properties=[
+                "physician_name",
+                "patient_name",
+                "text",
+                "hospital_name",
+            ],
+            embedding_node_property="embedding",
+        )
+    except Exception as e:
+        logger.error(f"Error setting up Neo4j vector index: {e}")
+        raise
 
     review_template = """
     Your job is to use patient reviews to answer questions about their experience at a hospital. 
@@ -67,32 +73,47 @@ def setup_vector_chain():
         messages=messages
     )
 
-    vector_chain = RetrievalQA.from_chain_type(
-        llm=ChatCohere(
-            model=HOSPITAL_QA_MODEL,
-            cohere_api_key=COHERE_API_KEY,
-            temperature=0.1
-        ),
-        chain_type="stuff",
-        retriever=neo4j_vector_index.as_retriever(k=10),
-    )
+    try:
+        vector_chain = RetrievalQA.from_chain_type(
+            llm=ChatCohere(
+                model=HOSPITAL_QA_MODEL,
+                cohere_api_key=COHERE_API_KEY,
+                temperature=0.1
+            ),
+            chain_type="stuff",
+            retriever=neo4j_vector_index.as_retriever(k=10),
+        )
 
-    vector_chain.combine_documents_chain.llm_chain.prompt = review_prompt
+        vector_chain.combine_documents_chain.llm_chain.prompt = review_prompt
+    except IndexError as e:
+        logger.error(f"IndexError in setting up vector chain: {e}")
+        raise
+    except Exception as e:
+        logger.error(f"Error setting up vector chain: {e}")
+        raise
 
     return vector_chain
+
 
 def run_query(vector_chain, query):
     logger.info("Starting query...")
     start_time = time.time()
-    response = vector_chain.invoke(query)
+    try:
+        response = vector_chain.invoke(query)
+    except Exception as e:
+        logger.error(f"Error during query execution: {e}")
+        raise
     end_time = time.time()
     response_time = end_time - start_time
     logger.info(f"Response time: {response_time} seconds")
-    return response
+    return response.get("result")
 
 if __name__ == "__main__":
     logger.info("Setting up...")
-    vector_chain = setup_vector_chain()
-    query = """What have patients said about hospital efficiency? Mention details from specific reviews."""
-    response = run_query(vector_chain, query)
-    logger.info(f"Response: {response}")
+    try:
+        vector_chain = setup_vector_chain()
+        query = """What have patients said about hospital efficiency? Mention details from specific reviews."""
+        response = run_query(vector_chain, query)
+        logger.info(f"Response: {response}")
+    except Exception as e:
+        logger.error(f"Error in main execution: {e}")
