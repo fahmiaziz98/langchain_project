d37051b59a67cfc708e37e9f9ac992a0c0ce185e
fahmiaziz98 <fahmiazizfadhil09@gmail.com>
2024-05-28 20:45:23 +0700

    logger

--- RAG/llm_api/src/chains/review_chain.py
+++ RAG/llm_api/src/chains/review_chain.py
@@ -28,6 +28,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 def create_embedding():
+    logger.info("Load CohereEmbedding...")
     return CohereEmbeddings(cohere_api_key=COHERE_API_KEY)
 
 def setup_neo4j_vector_index(embedding):
@@ -134,15 +135,15 @@ def run_query(vector_chain, query):
         logger.warning("No result found in response")
         return None
 
-if __name__ == "__main__":
-    logger.info("Setting up...")
-    try:
-        vector_chain = setup_vector_chain()
-        query = """What have patients said about hospital efficiency? Mention details from specific reviews."""
-        response = run_query(vector_chain, query)
-        if response:
-            print(response)
-        else:
-            print("No response received.")
-    except Exception as e:
-        logger.error(f"Error in main execution: {e}")
+# if __name__ == "__main__":
+#     logger.info("Setting up...")
+#     try:
+#         vector_chain = setup_vector_chain()
+#         query = """What have patients said about hospital efficiency? Mention details from specific reviews."""
+#         response = run_query(vector_chain, query)
+#         if response:
+#             print(response)
+#         else:
+#             print("No response received.")
+#     except Exception as e:
+#         logger.error(f"Error in main execution: {e}")
